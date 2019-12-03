4a9cca2e493d11275105c712a576c5c5f26ce6b6
debdutgoswami <debdutgoswami@gmail.com>
2019-12-03 15:48:37 +0530

    ADDED support for credentials in text file

--- secrets.py
+++ secrets.py
@@ -1,7 +1,17 @@
 """
-    Add your twitter handle's email and password in the respective fields (within the double quotes).
+    Add your twitter handle's email and password in the credentials.txt file.
     This will be used to automate the login.
 """
 
-email = ""
-password = ""
+def get_credentials() -> dict:
+    d = dict()
+    with open('credentials.txt') as f:
+        for line in f.readlines():
+            try:
+                key, value = line.split(": ")
+            except ValueError:
+                print('Add your email and password in credentials.txt file')
+                exit(0)
+            d[key] = value.rstrip(" \n")
+
+    return d
